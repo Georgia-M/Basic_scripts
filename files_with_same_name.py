83884b24b0baee75056af1ba97401520816e7ce0
Georgia-M <58853622+Georgia-M@users.noreply.github.com>
2020-11-20 23:15:12 +0200

    Create files_with_same_name.py

---
+++ files_with_same_name.py
@@ -0,0 +1,22 @@
+'''check if two files in different directories have the same name'''
+
+import os
+
+first_files_path = '...'
+second_files_path = '...'
+
+for r, d, f in os.walk(first_files_path):
+    for first_file in f:
+        if first_file.endswith(".txt"):
+            #keep only the name (without .txt)
+            base_first = os.path.basename(first_files_path+'\\'+first_file)
+            first_file_name = os.path.splitext(base_first)[0]
+
+            files = glob.glob(second_files_path+'\\*.dat')
+            for second_file in files:
+                #keep only the name
+                base_second = os.path.basename(second_file)
+                second_file_name = os.path.splitext(base_second)[0]
+
+                if second_file_name == first_file_name:
+                    print("same name")
